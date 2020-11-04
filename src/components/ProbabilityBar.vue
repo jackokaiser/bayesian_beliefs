47408b627e058cdbc5221f957543b6e142b2a147
Jacques KAISER <jackokaiser@gmail.com>
2020-11-04 19:11:04 +0100

    probability bar component

---
+++ src/components/ProbabilityBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bar" :style='style'>
+    <slot></slot>
+    {{ value }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProbabilityBar',
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  data: function () {
+    return {
+      style: { width: '' + this.value * 100 + '%' }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+ .bar {
+   background-color: red;
+ }
+</style>
--- src/pages/Question.vue
+++ src/pages/Question.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    question id: {{ questionId }}
+    {{ question.name }}
+    <div class="row"
+         v-for='hyp in question.hypothesis'
+         :key='hyp.id'>
+      <probability-bar :value="hyp.probability">
+        {{ hyp.name }}
+      </probability-bar>
+    </div>
+    <router-link :to="'/add-hypothesis'" tag="span" round>
+      <q-btn round color="primary" icon="add"/>
+    </router-link>
+    <router-link :to="'/add-evidence'" tag="span" round>
+      <q-btn round color="primary" icon="add"/>
+    </router-link>
   </div>
 </template>
 
 <script>
 import { LocalStorage } from 'quasar'
+import ProbabilityBar from 'components/ProbabilityBar.vue'
+
 export default {
   name: 'Question',
+  components: { ProbabilityBar },
   data: function () {
     var question = LocalStorage.getItem('question/' + this.questionId)
     return {
