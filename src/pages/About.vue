a090fd1df411f12f07140e185dfa75db5a4680d5
Jacques KAISER <jackokaiser@gmail.com>
2020-11-08 20:48:40 +0100

    more in about

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="column q-pa-md">
-    <h2> About </h2>
-    <h5> About the bayesian method</h5>
+    <div class="text-h2"> About </div>
+    <h5>About the bayesian method</h5>
     <p>
       The bayesian formula mathematically describes the probability of an hypothesis, given the probability of an event under that hypothesis.
-      It can be used iteratively.
-      The idea of this app comes from the <a target="_blank" href="https://www.youtube.com/watch?v=ZLI7OXoMQ9c&ab_channel=Hygi%C3%A8neMentale.">following video (French) from Hygiène mentale</a>.
+      It can be used iteratively to update and maintain a belief consistently.
+      The idea of this app comes from the <a target="_blank" href="https://www.youtube.com/watch?v=ZLI7OXoMQ9c&ab_channel=Hygi%C3%A8neMentale.">following video (French) from Christophe Michel (Hygiène mentale)</a>.
     </p>
     <q-separator/>
-    <h5> About the app</h5>
+    <h5>About the app</h5>
     <p>
       This app was developed with vue.js using quasar.
-      It is open-source on <a target="_blank" href="https://github.com/jackokaiser/bayesian_beliefs">github</a>.
+      It is deployed as an android app and a web app on <a target="_blank" href="https://jacqueskaiser.com/bayesian_beliefs">jacqueskaiser.com/bayesian_beliefs</a>.
+      The code is open-source under MIT license on <a target="_blank" href="https://github.com/jackokaiser/bayesian_beliefs">github</a>.
+    </p>
+    <p>
+      The data you enter in this app is yours.
+      It is only saved on your device using local storage.
+      It is <em>not</em> synchronized on a server (I do not rent a server).
     </p>
     <q-separator/>
-    <h5> About the developer</h5>
+    <h5>About the developer</h5>
     <p>
       I'm <a target="_blank" href="https://jacqueskaiser.com">Jacques Kaiser</a>.
     </p>
